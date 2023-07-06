a251f1e5132816b53f00d41cabeedd46530aa4cf
Yohwan Lim <dyghks7102@naver.com>
2023-07-06 18:54:03 +0900

    refactor. chapter02 2.5 BeforeEach 애노테이션과 인라인변수를 사용하여 리팩터링

--- src/test/java/com/yohwan/junitstudy/example/ProfileTest.java
+++ src/test/java/com/yohwan/junitstudy/example/ProfileTest.java
@@ -1,36 +1,39 @@
 package com.yohwan.junitstudy.example;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProfileTest {
+    private Profile profile;
+    private BooleanQuestion question;
+    private Criteria criteria;
+
+    @BeforeEach
+    void create() {
+        profile = new Profile("Bull Hockey, Inc. ");
+        question = new BooleanQuestion(1, "Got bonuses?");
+        criteria = new Criteria();
+    }
 
     @Test
+    @DisplayName("무조건 매치해야 되는 기준에 매치되지 않으면 false를 응답해야함")
     void matchAnswersFalseWhenMustMatchCriteriaNotMet() {
-        Profile profile = new Profile("Bull Hockey, Inc. ");
-        Question question = new BooleanQuestion(1, "Got bonuses?");
-        Answer profileAnswer = new Answer(question, Bool.FALSE);
-        profile.add(profileAnswer);
-        Criteria criteria = new Criteria();
-        Answer criteriaAnswer = new Answer(question, Bool.TRUE);
-        Criterion criterion = new Criterion(criteriaAnswer, Weight.MustMatch);
-        criteria.add(criterion);
+        profile.add(new Answer(question, Bool.FALSE));
+        criteria.add(new Criterion(new Answer(question, Bool.TRUE), Weight.MustMatch));
 
         boolean matches = profile.matches(criteria);
+
         assertFalse(matches);
     }
 
     @Test
+    @DisplayName("상관 없는 기준에 매치되면 true를 응답해야함")
     void matchAnswersTrueForAnyDontCareCriteria() {
-        Profile profile = new Profile("Bull Hockey, Inc. ");
-        Question question = new BooleanQuestion(1, "Got milk?");
-        Answer profileAnswer = new Answer(question, Bool.FALSE);
-        profile.add(profileAnswer);
-        Criteria criteria = new Criteria();
-        Answer criteriaAnswer = new Answer(question, Bool.TRUE);
-        Criterion criterion = new Criterion(criteriaAnswer, Weight.DontCare);
-        criteria.add(criterion);
+        profile.add(new Answer(question, Bool.FALSE));
+        criteria.add(new Criterion(new Answer(question, Bool.TRUE), Weight.DontCare));
 
         boolean matches = profile.matches(criteria);
         assertTrue(matches);
