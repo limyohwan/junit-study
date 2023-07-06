30ebdc11822363d9a8873bef957473e3baa0fc2e
Yohwan Lim <dyghks7102@naver.com>
2023-07-06 18:43:02 +0900

    feat. chapter02 2.3 단일 경로 커버, 2.4 두번째 테스트 만들기

---
+++ src/test/java/com/yohwan/junitstudy/example/ProfileTest.java
@@ -0,0 +1,39 @@
+package com.yohwan.junitstudy.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProfileTest {
+
+    @Test
+    void matchAnswersFalseWhenMustMatchCriteriaNotMet() {
+        Profile profile = new Profile("Bull Hockey, Inc. ");
+        Question question = new BooleanQuestion(1, "Got bonuses?");
+        Answer profileAnswer = new Answer(question, Bool.FALSE);
+        profile.add(profileAnswer);
+        Criteria criteria = new Criteria();
+        Answer criteriaAnswer = new Answer(question, Bool.TRUE);
+        Criterion criterion = new Criterion(criteriaAnswer, Weight.MustMatch);
+        criteria.add(criterion);
+
+        boolean matches = profile.matches(criteria);
+        assertFalse(matches);
+    }
+
+    @Test
+    void matchAnswersTrueForAnyDontCareCriteria() {
+        Profile profile = new Profile("Bull Hockey, Inc. ");
+        Question question = new BooleanQuestion(1, "Got milk?");
+        Answer profileAnswer = new Answer(question, Bool.FALSE);
+        profile.add(profileAnswer);
+        Criteria criteria = new Criteria();
+        Answer criteriaAnswer = new Answer(question, Bool.TRUE);
+        Criterion criterion = new Criterion(criteriaAnswer, Weight.DontCare);
+        criteria.add(criterion);
+
+        boolean matches = profile.matches(criteria);
+        assertTrue(matches);
+    }
+
+}
